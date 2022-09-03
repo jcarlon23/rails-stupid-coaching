1f2b48d1d3cee46945676cfb73d7f235a1cc7be2
James Carlon james.carlon1984@outlook.com <james.carlon1984@outlook.com>
2022-09-03 22:33:12 +0200

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,13 +7,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @question = "<%params[:questions] %>"
-    .include? "Hello"
-    @answer = "I don't care, get dressed and go to work!"
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
-
-
-
-
-
 end
